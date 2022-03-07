b77e5d6503bfcd79960e868bcb0c33f71b23fecd
aguilarcristy <cristyaguilar3101@gmail.com>
2022-03-07 13:57:45 -0800

    Adding Console

--- sketch.js
+++ sketch.js
@@ -1,31 +1,14 @@
+// Victims of Femicide in Mx
+
+let names = ["Ingrid Escamilla", "Fátima Aldrighetti", "Alicia Cortés"];
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  background(252, 3, 123);
+  console.log(names[0]);
+
 }
 
 function draw() {
-  background(255);
-  
-  fill(255, 179, 179);
-textSize(20);
-textFont('Georgia');
-text('Topic Idea for Data Visualization', 55, 50);
-  
-  // PINK CIRCLE
-  fill(255, 179, 179);
-  circle(200, 170, 100);
-  noStroke();
-  
-  // Eyes
-  fill(145, 132, 132);
-  circle(180, 155, 15); // Left
-  circle(220, 155, 15); // Right
-  
-  // Mouth
-
-
-  
-//arc(200, 180, 10, 20, 0, PI + QUARTER_PI, CHORD);
-//describe('white open arc with black outline with top right missing');
-
-
 }
